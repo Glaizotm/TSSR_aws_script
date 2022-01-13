2e0f94bb2d915ff1d12b51d6852c89170d25806f
maximeglaizot@gmail.com <maxime.glaizot@gmail.com>
2022-01-13 22:23:57 +0100

    fonctionel

--- CreateUser.sh
+++ CreateUser.sh
@@ -1,12 +1,48 @@
 #!/bin/bash
 
-fileList=$1
-for user in $(cat $fileList)
-do
-    echo $user
 
+fileList=$1
+pass="#MerciWf3!"
+for line in $(cat $fileList)
+do
+
+    user=$(echo $line |cut -d ',' -f1)
+    email=$(echo $line |cut -d ',' -f2)
+    # nom du fichier  qui contiendra les credential
+    credential="file_"$user"_credential"
+
+    # création de l'utilisateur
     aws iam create-user --user-name $user
-    aws iam create-access-key --user-name $user > "file_"$user"_credential"
+    # creation de l'acces key et sauvegarde des info dans le fichier credential
+    aws iam create-access-key --user-name $user > $credential
+
+    # récupération de la clef d'identification 
+
+    aws iam create-login-profile --user-name $user --password $pass
     aws iam add-user-to-group --group-name GroupeTest --user-name $user
+
+    # recupération des infos d'acces
+    keyId=$(grep KeyId $credential |cut -d: -f2 |sed "s/\ \"//g" |sed "s/\",//g")
+    secret=$(grep SecretAccessKey $credential |cut -d: -f2 |sed "s/\ \"//g" |sed "s/\",//g")
+
+
+    cat << EOF > tempMail
+To:$email
+Subject:Compte AWS
+
+Votre compte aws a été créé
+login : $user
+Passwd : $pass
+AccessKey : $keyId
+SecretKey : $secret
+
+EOF
+
+    sudo ssmtp $email < tempMail
+
+
 done
+
+
+
 
--- DelUser.sh
+++ DelUser.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 fileList=$1
-for user in $(cat $fileList)
+for line in $(cat $fileList)
 do
-    credential="file_"$user"_credential"
-    echo $credential
-    keyId=$(grep KeyId $credential |cut -d: -f2 |sed "s/\ \"//g" |sed "s/\",//g")  
-    echo $user $keyId
 
+    user=$(echo $line |cut -d ',' -f1)
+    email=$(echo $line |cut -d ',' -f2)
+    credential="file_"$user"_credential"
+    # récupératation de la clef dans le fichier json généré a la création
+    keyId=$(grep KeyId $credential |cut -d: -f2 |sed "s/\ \"//g" |sed "s/\",//g")  
+
+    #suppressier de l'utilisateur du groupe
     aws iam remove-user-from-group --group-name GroupeTest --user-name $user
+    # suppresssion de l'acces key  
     aws iam delete-access-key --user-name $user --access-key-id $keyId
+    
+    #suppression du login profile
+    aws iam delete-login-profile --user-name $user
+    # suppression de l'utilisateur
     aws iam delete-user --user-name $user 
+
+
+
+
 done
