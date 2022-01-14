c74e58c62977e8524f8ebd6293ac5974e2206635
maximeglaizot@gmail.com <maxime.glaizot@gmail.com>
2022-01-14 17:12:58 +0100

    ajout del start stop ec2

---
+++ DelEc2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ec2tags=$1
+listeFile="listeInstance"
+
+aws ec2 describe-instances --filters "Name=tag:Name,Values=*$ec2tags*" --query "Reservations[].Instances[].InstanceId" |sed '/\[/d' |sed '/\]/d' |sed 's/[",\,]//g' | sed 's/^[ \t]*//g' > $listeFile
+
+for instance in $(cat $listeFile) 
+do
+
+    echo $instance
+    aws ec2 terminate-instances --instance-ids $instance
+
+done
+
+rm $listeFile
